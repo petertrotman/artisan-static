69a17fa6bf35c3a443094288ce446479ce6bbdcd
Peter Trotman <petertrotman@gmail.com>
2020-09-11 19:35:01 +0100

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -19,31 +19,14 @@
 <body>
     <section>
         <header>
-            <nav>
-                <strong>{{ $page->site->title }}</strong><br>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/posts">Posts</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
+            PSimulations
         </header>
 
         <article>
             <section>
-                @yield('content')
+                <i>Coming Soon</i>
             </section>
         </article>
-
-        <footer>
-            <small>
-                &copy; <span data-year></span> {{ $page->owner->name }} &nbsp;&bull;&nbsp;
-                <a href="/feed.atom">RSS</a> &nbsp;&bull;&nbsp;
-                Maintained by Ranie Santos &nbsp;&bull;&nbsp;
-                <a href="https://github.com/raniesantos/artisan-static">GitHub repo</a>.
-            </small>
-        </footer>
     </section>
 
     <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
